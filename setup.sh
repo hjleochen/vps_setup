e51ae1707bcc00fa99a66585825d31d677423bde
H.J.LeoChen <hjleochen@hotmail.com>
2014-10-13 12:34:14 +0800

    chmod

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 sudo apt-get install build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev  -y
 sudo apt-get install libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion -y
 sudo apt-get install mysql-client screen mysql-server git-core -y
