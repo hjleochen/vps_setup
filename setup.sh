0c3cf56440327ab5ce2450f68544cd2ea6fe6960
H.J.LeoChen <hjleochen@hotmail.com>
2015-03-12 11:40:32 +0800

    add redis php

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 sudo apt-get install build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev  -y
 sudo apt-get install libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion -y
 sudo apt-get install mysql-client screen mysql-server git-core -y
+sudo apt-get install libreadline-dev libssl-dev libssl -y
+sudo apt-get install openjdk-7-jre-headless -y
 sudo apt-get install libmysqlclient-dev -y
 sudo apt-get install libicu-dev -y
 sudo apt-get install nginx -y
@@ -10,10 +12,17 @@ sudo apt-get install memcached -y
 sudo apt-get install imagemagick -y
 sudo apt-get install libmagickwand-dev -y
 sudo apt-get install redis-server -y
+sudo apt-get install zsh -y
+sudo apt-get install redis -y
+sudo apt-get install php5-fpm -y
+
+curl -L http://install.ohmyz.sh | sh
 
 sudo /usr/sbin/update-rc.d -f nginx defaults
 sudo /usr/sbin/update-rc.d -f mysql defaults
 sudo /usr/sbin/update-rc.d -f memcached defaults
+sudo /usr/sbin/update-rc.d -f redis defaults
+sudo /usr/sbin/update-rc.d -f php5-fpm defaults
 
 
 # 安装 rbenv
@@ -27,11 +36,15 @@ git clone git://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbe
 # 通过 rbenv update 命令来更新 rbenv 以及所有插件, 推荐
 git clone https://github.com/rkh/rbenv-update.git ~/.rbenv/plugins/rbenv-update
 
-echo '' >> .profile
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> .profile
-echo 'eval "$(rbenv init -)"' >> .profile
-echo '' >> .profile
-. ~/.profile
+
+echo '' >> .zshrc
+echo "bindkey -v" >>.zshrc
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> .zshrc
+echo 'eval "$(rbenv init -)"' >> .zshrc
+echo '' >> .zshrc
+
+/bin/zsh
+. ~/.zshrc
 
 rbenv install 2.1.1
 rbenv global 2.1.1
